0706ae0425950e59a6bcd80ccd4a668ac99db430
rishabhuserAKGEC <156677192+rishabhuserAKGEC@users.noreply.github.com>
2024-02-03 21:43:10 +0530

    Add files via upload

--- LearningBytes_9.c
+++ LearningBytes_9.c
@@ -15,13 +15,13 @@ int modify(int x)
 int main()
 {
 	int arr[10]={0,1,2,3,4,5,6,7,8,9};
-	int k,i,p;
 	printf("\nEntered elements of array are :\n");
 	for(i=0; i<10; i++)
 	{
-		printf("%d ",arr[i]);
+		printf("%d, ",arr[i]);
 		
 	}
+	
 	printf("\n");
 	for(i=0; i<10; i++)
 	{
